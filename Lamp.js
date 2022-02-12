b74c6d37b677e9a53e29f7571c625fcaa0e6b12a
ThatCoderKid726 <83380065+ThatCoderKid726@users.noreply.github.com>
2022-02-12 15:11:01 -0500

    Add files via upload

---
+++ Lamp.js
@@ -0,0 +1,30 @@
+Status = "";
+Lamp_image = "";
+
+function preload(){
+    Lamp_image = loadImage("Lamp.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(Lamp_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(Lamp_image,0,0,640,350);
+}
